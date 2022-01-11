33e82a62eec6c217925f56dd2e557a17cf17b0b7
Mihail <mwxood@users.noreply.github.com>
2022-01-11 11:10:46 +0200

    added some elements

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Container, Button, TextField, Avatar, Card, CardContent   } from '@mui/material';
+import { Button, TextField, Avatar, Grid, Paper, Box   } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import './login.css';
 
 const Login = () => {
 
@@ -28,19 +30,26 @@ const Login = () => {
 
     return(
         <div>
-            <Container>
-                <Card>
-                    <CardContent>
-                        <Avatar />
-                    </CardContent>
-                </Card>
-               
-                <form onSubmit={onLogin}>
-                    <TextField type="text" value={userInput} label="Username" onChange={addUser}  variant="standard"/>
-                    <TextField type="text" value={passwordInput} label="Password" onChange={addPassword}  variant="standard"/>
-                    <Button type="submit"  variant="contained">send</Button>
-                </form>
-            </Container>
+            <Grid>
+                <Paper elevation={10} className='paperClass'>
+                    <Grid className="loginGrid">
+                        <Avatar className="avatar">
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <h2 className='login-title'>Sign In</h2>
+                    </Grid>
+                   
+                    <form onSubmit={onLogin}>
+                       <Box mb={1}>
+                         <TextField className="fullWidth" type="text" value={userInput} label="Username" onChange={addUser}  variant="standard"/>
+                       </Box>
+                       <Box mb={3}>
+                            <TextField className="fullWidth" type="text" value={passwordInput} label="Password" onChange={addPassword}  variant="standard"/>
+                       </Box>
+                        <Button className="fullWidth" type="submit"  variant="contained">send</Button>
+                    </form>
+                </Paper>
+            </Grid>
         </div>
     );
 }
